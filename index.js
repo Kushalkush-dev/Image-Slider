71fec74ae1631f69374ec51978f9ebfb9abcf277
Kushal J <jkushal208@gmail.com>
2025-03-07 16:50:22 +0530

    completed

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const images=document.querySelectorAll(".images img");
 let indeximg=0;
-document.addEventListener("DOMContentLoaded",()=>initial(indeximg));
+document.addEventListener("DOMContentLoaded",()=>initial());
+let timeoutid;
 
- function initial(index){
-  images[index].classList.add("enabled")
+ function initial(){
+  images[indeximg].classList.add("enabled");
+  if(images.length>0){
+    timeoutid=setInterval(nextimg,5000);
+
+  }
+  
 
  }
 
+
+ function showimg(){
+
+  images.forEach(image=>{image.classList.remove("enabled")})
+ 
+    if(indeximg>=images.length){
+      indeximg=0;
+    }else if(indeximg<0){
+      indeximg=images.length-1;
+    }
+
+      images[indeximg].classList.add("enabled")
+  }
+    
+
+ 
+
+ function nextimg(){
+  indeximg++;
+  showimg()
+ }
+
+ function previmg(){
+  indeximg--;
+  showimg()
+  clearInterval(timeoutid)
+
+ }
